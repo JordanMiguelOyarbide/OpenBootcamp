4810d4da7f2e0ec4f5bc7f8b22454bfba0ddbc77
JordanMiguelOyarbide <118465802+JordanMiguelOyarbide@users.noreply.github.com>
2023-02-21 17:33:20 +0100

    Update index.js

--- JAVASCRIPT/05-NUMEROS/index.js
+++ JAVASCRIPT/05-NUMEROS/index.js
@@ -8,9 +8,10 @@ let alturaMetros = 1.72;
 let peso = parseFloat(80,7);
 
 //- Una variable que contenga tu altura en metros redondeada hacia arriba
-let alturaRedondeada = Math.round(alturaMetros);
+let alturaRedondeada = Math.ceil(alturaMetros);
 
 //- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
-let pesoRedondeado = Math.min(peso);
+let pesoRedondeado = Math.floor(peso);
 
 //- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
