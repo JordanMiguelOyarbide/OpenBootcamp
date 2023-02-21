54ed52d99fac38d1162901e7013ab383a438991f
JordanMiguelOyarbide <118465802+JordanMiguelOyarbide@users.noreply.github.com>
2023-02-21 11:36:04 +0100

    Add files via upload

---
+++ JAVASCRIPT/02-SYNTAX/index.js
@@ -0,0 +1,21 @@
+//- Tu nombre (string)
+let nombre = "Jordan Oyarbide";
+
+//- Tu edad (number)
+let edad = 27;
+
+//- ¿Eres desarrollador? (boolean)
+let desarrollador = false;
+
+//- Tu fecha de nacimiento (Date)
+let fechaNacimiento = new Date(1995, 5, 3);
+
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+var libroFavorito = {
+    titulo: "Vray",
+    autor: "Rob Jackson",
+    fecha: 2022,
+    url: "vray.com",
+};
+
+console.log(libroFavorito);
