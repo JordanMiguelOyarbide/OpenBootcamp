111f6416734ee57d38042e86d721f8bb836faae5
JordanMiguelOyarbide <118465802+JordanMiguelOyarbide@users.noreply.github.com>
2023-02-21 17:29:08 +0100

    Add files via upload

---
+++ JAVASCRIPT/05-NUMEROS/index.js
@@ -0,0 +1,16 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+let alturaCentimetros = 172;
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaMetros = 1.72;
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = parseFloat(80,7);
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = Math.round(alturaMetros);
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeado = Math.min(peso);
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
