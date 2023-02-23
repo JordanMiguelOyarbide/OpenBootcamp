c7cda4d1ece7e17270c9a041a05c301e3a9b8400
JordanMiguelOyarbide <118465802+JordanMiguelOyarbide@users.noreply.github.com>
2023-02-23 09:42:28 +0100

    Add files via upload

---
+++ JAVASCRIPT/08-FUNCTION/index.js
@@ -0,0 +1,16 @@
+//- Una función sin parámetros que devuelva siempre "true"
+function verdadero() {
+   return (true);
+}
+//- Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+async function damePromesa () {
+    return setTimeout(() => console.log("hola es una promesa"), 5000)
+}
+
+//- Una función generadora de índices pares automáticos
+function* generaIndice() {
+    let id = 0;
+    while(true) {
+        yield id += 2;S
+    }
+}
