b3fc370d2e1f8d898afb9041896c32909861a270
JordanMiguelOyarbide <118465802+JordanMiguelOyarbide@users.noreply.github.com>
2023-02-24 20:43:59 +0100

    Update index.js

--- JAVASCRIPT/12-DEPURACION/index.js
+++ JAVASCRIPT/12-DEPURACION/index.js
@@ -1,8 +1,12 @@
-//Crea un nuevo fichero JS que contenga las siguientes líneas
-
-//- Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
-const acu = [1, 1];
+const acu = [];
 function Fibonacci(num) {
+    if (num == 1) {
+        return acu.push(1);
+    } 
+    if (num == 2) {
+        return acu.push(1, 1);
+    }
+    acu.push(1, 1);
     for (let i = 1; i < (num - 1); i++) {
         let sumar = acu[(i - 1)] + acu[i];
         acu.push(sumar);
@@ -11,4 +15,3 @@ function Fibonacci(num) {
 
 Fibonacci(6);
 console.log(acu);
-//- Ejecuta la depuración de VSCode para visualizar la ejecución de la función
